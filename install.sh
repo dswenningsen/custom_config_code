ff643935b76c7a3edf5e124b4651aefafee0ed0e
root <root@localhost.localdomain>
2020-07-28 11:16:03 -0700

    Install script added

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cp bash_stuff/.bash* $HOME/
+cp bash_stuff/.bash* /root/
+
+cp msfconsole_stuff/msfconsole.rc $HOME/
+cp msfconsole_stuff/msfconsole.rc /root/
+
+cp tmux_stuff/.tmux.conf $HOME/
+cp tmux_stuff/.tmux.conf /root/
+
+cp z/z.sh /bin/
+
