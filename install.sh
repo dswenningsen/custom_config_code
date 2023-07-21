115267fd9dddd564f874b4d035edb648940c0a20
dswenningsen <dswenningsen@gmail.com>
2023-07-21 10:19:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+curl -s https://ohmyposh.dev/install.sh | bash -s
+wget  https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip
+cp CascadiaCode.zip /usr/share/fonts/
+echo "Done copying to /usr/share/fonts"
+unzip /usr/share/fonts/CascadiaCode.zip
+rm /usr/share/fonts/CascadiaCode.zip
+fc-cache -fv
+cp .mytheme.omp.json ~/
+
+
 cp bash_stuff/.bash* $HOME/
 cp bash_stuff/.bash* /root/
 
